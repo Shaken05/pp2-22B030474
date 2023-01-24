1152757697af2b1fab7f7ead058840a0504b63f6
Shaken05 <s_shynggyssuly@kbtu.kz>
2023-01-24 15:41:18 +0600

    python operators

---
+++ python_operators.py
@@ -0,0 +1,56 @@
+'''print(10 + 5)
+#Python Arithmetic Operators:
+#Addition:
+'+'
+#Substraction:
+'-'
+#Multiplication:
+'*'
+#Division:
+'/'
+#Modulus:
+'%'
+#Exponentation:
+'**'
+#Floor division:
+'//'
+
+#Python Assignment Operators:
+'='	    x = 5	x = 5	
+'+='	x += 3	x = x + 3	
+'-='	x -= 3	x = x - 3	
+'*='	x *= 3	x = x * 3	
+'/='	x /= 3	x = x / 3	
+'%='	x %= 3	x = x % 3	
+'//='	x //= 3	x = x // 3	
+'**='	x **= 3	x = x ** 3	
+'&='	x &= 3	x = x & 3	
+'|='	x |= 3	x = x | 3	
+'^='	x ^= 3	x = x ^ 3	
+'>>='	x >>= 3	x = x >> 3	
+'<<='	x <<= 3	x = x << 3
+
+#Python Comparison Operators:
+'=='	'Equal'	x == y	
+'!='	'Not equal'	x != y	
+'>'	'Greater than'	x > y	
+'<'	'Less than'	x < y	
+'>='	'Greater than or equal to'	x >= y	
+'<='	'Less than or equal to'	x <= y
+
+#Python Logical Operators:
+or
+not
+
+#Python Membership Operators
+in
+not in
+
+Python Bitwise Operators
+& 	AND	Sets each bit to 1 if both bits are 1
+|	OR	Sets each bit to 1 if one of two bits is 1
+^	XOR	Sets each bit to 1 if only one of two bits is 1
+~	NOT	Inverts all the bits
+<<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off
+>>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
+'''
