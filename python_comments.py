77849a43ffd79c4e7f5267d1751bb9484f16ce31
Shaken05 <s_shynggyssuly@kbtu.kz>
2023-01-24 11:33:55 +0600

    python comments

---
+++ python_comments.py
@@ -0,0 +1,20 @@
+#Comments starts with a #, and Python will ignore them:
+#This is a comment
+print("Hello, World!")
+#Comments can be placed at the end of a line, and Python will ignore the rest of the line:
+print("Hello, World!") #This is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
